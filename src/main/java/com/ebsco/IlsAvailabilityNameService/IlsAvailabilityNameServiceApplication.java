c11767bbe473cd40ac43b0800c089360413d6926
dlees <dlees@ebsco.com>
2018-04-08 17:49:16 -0400

    Second inital commit :)

---
+++ src/main/java/com/ebsco/IlsAvailabilityNameService/IlsAvailabilityNameServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ebsco.IlsAvailabilityNameService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlsAvailabilityNameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlsAvailabilityNameServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ebsco/IlsAvailabilityNameService/IlsAvailabilityNameServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ebsco.IlsAvailabilityNameService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IlsAvailabilityNameServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
